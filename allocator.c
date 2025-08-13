a133513a76d056d2142290636fa7b9fda5cf7b9f
Rushi <rushiadicherla@gmail.com>
2025-08-13 21:54:19 +0530

    Update allocator.c

--- allocator.c
+++ allocator.c
@@ -1,61 +1,69 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <stddef.h>
-#include <string.h>
 
-#define POOL_KB    100
-#define POOL_SIZE  (POOL_KB * 1024)      
-#define MIN_ALLOC  1                     
-#define ALIGNMENT  (sizeof(int))         
-#define TRUE  1
-#define FALSE 0
+#define POOL_SIZE (100 * 1024)
+static uint8_t memory_pool[POOL_SIZE];
+typedef struct Block {
+    int size;               
+    int free;              
+    struct Block *next;    
+} Block;
 
-  typedef struct block_header {
-    size_t size;                
-    int free;                    
-    struct block_header *next;   
-} block_header_t;
+static Block *free_list = NULL;
 
-int *allocate(int size) {
+void init_memory() {
+    free_list = (Block*)memory_pool;
+    free_list->size = POOL_SIZE - sizeof(Block);
+    free_list->free = 1;
+    free_list->next = NULL;
+}
+
+int* allocate(int size) {
     if (size <= 0) return NULL;
-    if (size < MIN_ALLOC) size = MIN_ALLOC;
 
-    size_t req_size = ALIGN_UP((size_t)size, ALIGNMENT);
-
-    
-    block_header_t *blk = find_first_fit(req_size);
-    if (!blk) {
+    Block *curr = free_list;
+    while (curr) {
+        if (curr->free && curr->size >= size) {
       
-        coalesce_if_possible();
-        blk = find_first_fit(req_size);
-        if (!blk) return NULL;
+            if (curr->size > size + sizeof(Block)) {
+                Block *new_block = (Block*)((uint8_t*)curr + sizeof(Block) + size);
+                new_block->size = curr->size - size - sizeof(Block);
+                new_block->free = 1;
+                new_block->next = curr->next;
+                curr->next = new_block;
+                curr->size = size;
+            }
+            curr->free = 0;
+            return (int*)((uint8_t*)curr + sizeof(Block));
+        }
+        curr = curr->next;
     }
-
-    
-    if (blk->size >= req_size) {
-        
-        split_block(blk, req_size);
-    } else {
-      
-        return NULL;
-    }
-
-    uint8_t *user_mem = (uint8_t *)blk + HEADER_SIZE;
-    return (int *)user_mem;
+    return NULL; 
 }
 
 void deallocate(int *ptr) {
     if (!ptr) return;
+    Block *block = (Block*)((uint8_t*)ptr - sizeof(Block));
+    block->free = 1;
 
-    block_header_t *hdr = ptr_to_header((void *)ptr);
-    if (!hdr) {
-        return;
+    if (block->next && block->next->free) {
+        block->size += sizeof(Block) + block->next->size;
+        block->next = block->next->next;
     }
+}
 
-    if (hdr->free) {
-        return;
-    }
+int main() {
+    init_memory();
 
-    hdr->free = TRUE;
-    coalesce_if_possible();
+    int *mem[100];
+    mem[0] = allocate(128);
+    mem[1] = allocate(1024);
+
+    deallocate(mem[1]);
+    mem[1] = allocate(512);
+
+    printf("mem[0]: %p\n", mem[0]);
+    printf("mem[1]: %p\n", mem[1]);
+
+    return 0;
 }
